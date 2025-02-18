9a9d4ae6cd369f7f7c7636b71b009be579f51ca2
Shabbir Ahamed <shabbirkimgbrand55@gmail.com>
2025-02-18 11:02:59 +0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-4xl'>working</h1>
+      <h1 className='text-red-600'>its fixed</h1>
+    </>
+  )
+}
+
+export default App
